11621111e9168bd621c81450151decfa8ef2ba9d
patilnikhil805 <pnikhil.805@gmail.com>
2024-08-13 16:34:27 -0400

    1:29:20

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,18 +1,34 @@
 'use client'
 
 import React from 'react'
-import { Menu,Bell, Sun, Settings } from 'lucide-react'
+import { Menu,Bell, Sun, Settings, Moon } from 'lucide-react'
 import Link from 'next/link'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state'
 type Props = {}
 
 const Navbar = () => {
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+        (state) => state.global.isSidebarCollapsed
+      );
+      const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+    const toggleSidebar = () => {
+        dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    const toggleDarkMode = () => {
+        dispatch(setIsDarkMode(!isDarkMode));
+      };
+    
   return (
     <div className="flex justify-between items-center w-full mb-7">
         {/* LEFT SIDE */}
         <div className="flex justify-between items-center gap-5">
             <button
                 className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-                onClick={() => {}}
+                onClick={toggleSidebar}
                 >
                 <Menu className="w-4 h-4" />
             </button>
@@ -31,8 +47,12 @@ const Navbar = () => {
         <div className="flex justify-between items-center gap-5">
             <div className="hidden md:flex justify-between items-center gap-5">
                 <div>
-                    <button onClick={() => {}}>
-                        <Sun className="cursor-pointer text-gray-500" size={24} />
+                    <button onClick={toggleDarkMode}>
+                    {isDarkMode ? (
+                            <Sun className="cursor-pointer text-gray-500" size={24} />
+                        ) : (
+                            <Moon className="cursor-pointer text-gray-500" size={24} />
+                        )}
                     </button>
                 </div>
                 <div className="relative">
