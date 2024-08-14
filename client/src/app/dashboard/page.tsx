193b21dbba1b5938950e479ea8ca4f4e3bd4b23b
patilnikhil805 <pnikhil.805@gmail.com>
2024-08-14 11:02:10 -0400

    2:38

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import React from 'react'
 
 type Props = {}
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      Dashboard
+    </div>
   )
 }
 
